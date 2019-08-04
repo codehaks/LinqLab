31315cf5386eee875d4ba3ad345d0b0fb0ec12aa
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-04 15:41:46 +0430

    fix

--- LinqLab/Program.cs
+++ LinqLab/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
+using System.Linq;
 
 namespace LinqLab
 {
@@ -7,6 +9,21 @@ namespace LinqLab
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            TestZip();
+
+        }
+
+        static void TestZip()
+        {
+            int[] v1 = { 1, 2, 3 };
+            int[] v2 = { 5, 6, 7 };
+
+            var r=v1.Zip(v2, (a, b) => a * b);
+
+            r.ForEach((x) =>
+            {
+                Console.WriteLine(x);
+            });
         }
     }
 }
